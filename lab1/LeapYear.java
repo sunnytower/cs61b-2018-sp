c17346b18ca8795b95cedcc001880246573cda5e
sunnytower <ljxreal1@gmail.com>
2022-06-28 19:42:57 +0800

    lab1 completed.

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,9 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year){
+        return ((year%4==0&&year%100!=0)||year%400==0);
+    }
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
